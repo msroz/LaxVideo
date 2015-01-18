238639247ada8f39277afbf1182c122aa6b8938c
msroz <masaru95@gmail.com>
2015-01-18 16:04:30 +0900

    Remove unused logic

--- lib/Lax/Model/Video.pm
+++ lib/Lax/Model/Video.pm
@@ -9,6 +9,7 @@ sub collection {
     my $self = shift;
     my $args = args(
         +{
+            page    => 1,
             sort_by => 0,
             order   => 0,
         }, @_
@@ -17,81 +18,7 @@ sub collection {
     my $rows = $self->model('Data::Video')->search();
     return [] unless @{$rows};
 
-    my $sort_by = $args->{sort_by};
-    my $order   = $args->{order};
-    return [ map { $_->get_columns } @{$rows} ] unless($sort_by && $order);
-
-    my @row;
-    if ($sort_by eq 'created_at') {
-        @row = order_by_created_at(videos => $rows, order => $order);
-    }
-    elsif($sort_by eq 'duraton') {
-        @row = order_by_duration(videos => $rows, order => $order);
-    }
-    elsif($sort_by eq 'like') {
-        @row = order_by_like(videos => $rows, order => $order);
-    }
-    else {
-        die "unknown sort_by: $sort_by";
-    }
-
-}
-
-sub order_by_created_at {
-    my $self = shift;
-    my $args = args(+{ videos => 1, order => 1 }, @_);
-
-    my @rows;
-    if ($args->{order} eq 'asc') {
-        @rows =
-            map { $_->[1] } sort { $a->[0] <=> $b->[0] } map { [ $_->created_at, $_ ] } @{$args->{videos}};
-    }
-    elsif ($args->{order} eq 'desc') {
-        @rows =
-            map { $_->[1] } sort { $b->[0] <=> $a->[0] } map { [ $_->created_at, $_ ] } @{$args->{videos}};
-    }
-    else {
-        die "unknown order: $args->{order}";
-    }
-    return \@rows;
-}
-
-sub order_by_duration {
-    my $self = shift;
-    my $args = args(+{ videos => 1, order => 1 }, @_);
-
-    my @rows;
-    if ($args->{order} eq 'asc') {
-        @rows =
-            map { $_->[1] } sort { $a->[0] <=> $b->[0] } map { [ $_->duration, $_ ] } @{$args->{videos}};
-    }
-    elsif ($args->{order} eq 'desc') {
-        @rows =
-            map { $_->[1] } sort { $b->[0] <=> $a->[0] } map { [ $_->duration, $_ ] } @{$args->{videos}};
-    }
-    else {
-        die "unknown order: $args->{order}";
-    }
-    return \@rows;
-}
-
-sub order_by_like {
-    my $self = shift;
-    my $args = args(+{ videos => 1, order => 1 }, @_);
-
-    my @rows;
-    if ($args->{order} eq 'asc') {
-        @rows =
-            map { $_->[1] } sort { $a->[0] <=> $b->[0] } map { [ $_->like, $_ ] } @{$args->{videos}};
-    }
-    elsif ($args->{order} eq 'desc') {
-        @rows =
-            map { $_->[1] } sort { $b->[0] <=> $a->[0] } map { [ $_->like, $_ ] } @{$args->{videos}};
-    }
-    else {
-        die "unknown order: $args->{order}";
-    }
-    return \@rows;
+    return [ map { $_->get_columns } @{$rows} ];
 }
 
 1;
--- lib/Lax/Web/Controller/Root.pm
+++ lib/Lax/Web/Controller/Root.pm
@@ -9,13 +9,23 @@ sub index {
     my ($self, $c, $matched) = @_;
 
     my $v = $self->form_validator($c);
-    $v->check(
+    my $res = $v->check(
         page    => [qw/UINT/],
         order   => [qw//],
         sort_by => [qw//],
     );
+    if ($v->has_error) {
+        return $c->res_404; # TODO: res_400 bad request
+    }
+    my $page    = $res->query->param('page')    || 1;
+    my $order   = $res->query->param('order')   || 'desc';
+    my $sort_by = $res->query->param('sort_by');
 
-    my $rows = $self->model('Video')->collection();
+    my $rows = $self->model('Video')->collection(
+        page  => $page,
+        order => $order,
+        (sort_by => $sort_by) x !!$sort_by,
+    );
 
     my $tmple_params = +{
         list => $rows,
